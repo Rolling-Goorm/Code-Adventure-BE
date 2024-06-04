1e5e57a0d51b992a796aadb70a62c6052333b373
jae woo Jung <awfjol2008@gmail.com>
2024-06-05 02:45:22 +0900

    feat: 사용자 엔티티 구현

---
+++ src/main/java/com/goorm/codeAdventure/user/domain/User.java
@@ -0,0 +1,39 @@
+package com.goorm.codeAdventure.user.domain;
+
+import com.goorm.codeAdventure.game.domain.Language;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Table(name = "users")
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String loginId;
+
+    private String loginPassword;
+
+    private String name;
+
+    private String nickname;
+
+    private Language prefferedLanguage;
+
+    private LocalDate birth;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private int coin;
+
+    private int latestStage;
+}

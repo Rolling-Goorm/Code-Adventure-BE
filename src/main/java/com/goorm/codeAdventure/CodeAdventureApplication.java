36ba07a75310b03ad838d8c482ba014c58edac1b
jae woo Jung <awfjol2008@gmail.com>
2024-06-05 02:43:28 +0900

    chore: 프로젝트 초기 생성

---
+++ src/main/java/com/goorm/codeAdventure/CodeAdventureApplication.java
@@ -0,0 +1,13 @@
+package com.goorm.codeAdventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeAdventureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeAdventureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goorm/codeAdventure/CodeAdventureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goorm.codeAdventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeAdventureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

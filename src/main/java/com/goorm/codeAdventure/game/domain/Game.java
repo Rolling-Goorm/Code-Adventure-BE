51a0fc937d647bc4be9f9d014880608425c80be0
jae woo Jung <awfjol2008@gmail.com>
2024-06-05 02:46:13 +0900

    feat: 게임 엔티티 구현

---
+++ src/main/java/com/goorm/codeAdventure/game/domain/Game.java
@@ -0,0 +1,25 @@
+package com.goorm.codeAdventure.game.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name = "games")
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Language language;
+
+    @OneToMany(mappedBy = "game")
+    private List<Part> parts;
+}

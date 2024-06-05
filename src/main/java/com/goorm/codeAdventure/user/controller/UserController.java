5294b0e0cba7b0dbf55d1f021faf5978c90246e5
minseob <minseob15@naver.com>
2024-06-05 23:05:19 +0900

    test1

--- src/main/java/com/goorm/codeAdventure/user/controller/UserController.java
+++ src/main/java/com/goorm/codeAdventure/user/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.goorm.codeAdventure.user.controller;
 
+import com.goorm.codeAdventure.user.domain.User;
 import com.goorm.codeAdventure.user.userService.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +16,35 @@ public class UserController {
 
     private final UserService userService;
 
+    /**
+     *
+     */
+    @GetMapping("/users/new")
+    public String createForm(Model model) {
+        model.addAttribute("userForm", new UserForm());
+        return "users/createUserForm";
+    }
+
+    @PostMapping("/users/new")
+    public String create(@Valid UserForm userForm, BindingResult result) {
+        if (result.hasErrors()) {
+            return "users/createUserForm";
+        }
+
+        // userForm을 가지고 입력을 받은 내용을 다시 Entity인 User객체에 생성자로 정보를 담는다.
+        User user = new User(
+                userForm.getLoginId(),
+                userForm.getLoginPassword(),
+                userForm.getName(),
+                userForm.getNickname(),
+                userForm.getPrefferedLanguage(),
+                userForm.getBirth(),
+                userForm.getEmail(),
+                userForm.getPhoneNumber()
+        );
+
+        userService.join(user);
+        return "users/login"; // 로그인 화면창으로?
+    }
 
 }

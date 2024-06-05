8fe0c0b23ba783951f9a3835fa49a90a967d50b8
Park, minsu <cmwmss59@gmail.com>
2024-06-05 17:06:47 +0900

    feat: CoinRepository 구성

--- src/main/java/com/goorm/codeAdventure/Repository/CoinRepository.java
+++ src/main/java/com/goorm/codeAdventure/Repository/CoinRepository.java
@@ -5,5 +5,5 @@ import com.goorm.codeAdventure.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CoinRepository extends JpaRepository<User,Long> {
-    //User에 대한 db 접근 처리 .
+    //User에 대한 db 접근 처리하는 부분 .
 }

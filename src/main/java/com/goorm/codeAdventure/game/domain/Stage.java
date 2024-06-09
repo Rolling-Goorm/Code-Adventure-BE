6d5fc4e7ff7e113750a4c0e0ad2ef789cee740b2
jae woo Jung <awfjol2008@gmail.com>
2024-06-09 15:59:03 +0900

    feat: 각 분류별 스테이지 관련 Stage 엔티티 구현

---
+++ src/main/java/com/goorm/codeAdventure/game/domain/Stage.java
@@ -0,0 +1,38 @@
+package com.goorm.codeAdventure.game.domain;
+
+import com.goorm.codeAdventure.problem.domain.Problem;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "stages")
+public class Stage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String level; // 예: Easy, Medium, Hard
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @OneToOne(mappedBy = "stage")
+    private Problem problem;
+
+    /**
+     * 연관관계 설정 관련 메서드
+     */
+    public void setCategory(Category category) {
+        this.category = category;
+        category.getStages().add(this);
+    }
+}

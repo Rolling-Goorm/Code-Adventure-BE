13da205548936095f20a97ffed8c6297fba19ce8
jae woo Jung <awfjol2008@gmail.com>
2024-06-09 20:52:39 +0900

    feat: Stage 엔티티에 진행현황 목록 필드 추가

--- src/main/java/com/goorm/codeAdventure/game/domain/Stage.java
+++ src/main/java/com/goorm/codeAdventure/game/domain/Stage.java
@@ -7,9 +7,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -27,6 +31,9 @@ public class Stage {
 
     @OneToOne(mappedBy = "stage")
     private Problem problem;
+
+    @OneToMany(mappedBy = "stage")
+    private List<Progress> progresses = new ArrayList<>();
 
     /**
      * 연관관계 설정 관련 메서드

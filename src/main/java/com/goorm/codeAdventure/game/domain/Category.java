f48cbb97cc0b9eaf79ea46c6dbb8deee7ce3ca09
jae woo Jung <awfjol2008@gmail.com>
2024-06-09 15:57:45 +0900

    feat: 알고리즘 분류 관련 Category 엔티티 구현

---
+++ src/main/java/com/goorm/codeAdventure/game/domain/Category.java
@@ -0,0 +1,28 @@
+package com.goorm.codeAdventure.game.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Stage> stages = new ArrayList<>();
+
+    // getters and setters
+}

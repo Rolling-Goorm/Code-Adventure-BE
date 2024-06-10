a1b8079fa3b799d84a25f8938a291434bfa08bae
jae woo Jung <awfjol2008@gmail.com>
2024-06-10 16:47:02 +0900

    refactor: 패키지 재구성 2

--- src/main/java/com/goorm/codeAdventure/user/repository/UserRepository.java
+++ src/main/java/com/goorm/codeAdventure/user/repository/UserRepository.java
@@ -1,11 +1,7 @@
 package com.goorm.codeAdventure.user.repository;
 
-import com.goorm.codeAdventure.domain.user.entity.User;
-
+import com.goorm.codeAdventure.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
-
 public interface UserRepository extends JpaRepository<User,Long> {
-    //User에 대한 db 접근 처리하는 부분 .
 }

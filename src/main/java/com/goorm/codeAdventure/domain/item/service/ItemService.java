1368fc8df189ac9b2e30e4d9577e23afb3137d20
jae woo Jung <awfjol2008@gmail.com>
2024-06-11 01:51:39 +0900

    refactor: 사용자 엔티티 단건 조회 메서드명 변경

--- src/main/java/com/goorm/codeAdventure/domain/item/service/ItemService.java
+++ src/main/java/com/goorm/codeAdventure/domain/item/service/ItemService.java
@@ -71,8 +71,7 @@ public class ItemService {
         {
             throw new RuntimeException("재고가 없습니다");
         }
-        User user = userRepository.findById(userId)
-                .orElseThrow(IllegalArgumentException::new);
+        User user = userRepository.findById(userId);
         //구매 로직
         Integer totalprice = item.minusStock(request.getQuantity());//남은 재고 저장 했음
         int usedCoin = user.getCoin() - totalprice;

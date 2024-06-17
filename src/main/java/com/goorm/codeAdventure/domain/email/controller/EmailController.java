d72989ba2e2cc6e4781f20ba971a48d4033eef5f
Park, minsu <cmwmss59@gmail.com>
2024-06-17 10:27:40 +0900

    feat : email 전송 기능 개발 application.yml : smtp 서버를 이용하기 위해 계정 설정 application.properties: Jpa repo에 연결하기 위한 db 정보와 포트 번호 설정 build.gradle: mail 의존성 추가 EmailController: email 요청 구성 EmailService:java Mail Sender구성 gift.jpg : 선물 이미지 구성.

---
+++ src/main/java/com/goorm/codeAdventure/domain/email/controller/EmailController.java
@@ -0,0 +1,43 @@
+package com.goorm.codeAdventure.domain.email.controller;
+
+import com.goorm.codeAdventure.domain.email.entity.EmailMessage;
+import com.goorm.codeAdventure.domain.email.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class EmailController {
+
+    private final EmailService emailService;
+
+    @PostMapping("/send-mail")
+    public ResponseEntity<EmailMessage> sendMail() {
+        EmailMessage emailMessage = EmailMessage.builder()
+                .to("cmwmss59@gmail.com")
+                .subject("테스트 메일 제목")
+                .message("""
+                        <!DOCTYPE html>
+                        <html lang="en" >
+                        <head>
+                            <meta charset="UTF-8">
+                            <title>메일 테스트</title>
+                        </head>
+                        <body>
+                        <div>
+                            <p>안녕하세요, <span ></span>손님</p>
+                            <p>메일 테스트입니다.</p>
+                        </div>
+                        <div>
+                            <img src="gift.jpg" height="630" width="450" alt="이미지 오류"/>
+                        </div>
+                        </body>
+                        </html>">테스트 메일 본문<div></body></html>""")
+                .build();
+        emailService.sendMail(emailMessage);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}

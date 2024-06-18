3d3cfc475dd9aa82be7380a860bafbe0eda6f493
jae woo Jung <awfjol2008@gmail.com>
2024-06-18 18:09:55 +0900

    feat: 로그인한 유저를 인식해서 해당 유저의 진행도를 가져오도록 기능 추가

--- src/main/java/com/goorm/codeAdventure/domain/game/controller/GameController.java
+++ src/main/java/com/goorm/codeAdventure/domain/game/controller/GameController.java
@@ -5,11 +5,13 @@ import com.goorm.codeAdventure.domain.game.dto.response.ProgrammingLanguageRespo
 import com.goorm.codeAdventure.domain.game.dto.response.StageResponse;
 import com.goorm.codeAdventure.domain.game.service.GameService;
 import com.goorm.codeAdventure.domain.game.service.ProgressService;
+import com.goorm.codeAdventure.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import java.util.List;
 import java.util.stream.IntStream;
@@ -21,12 +23,14 @@ public class GameController {
     private final ProgressService progressService;
 
     @GetMapping("/programmingLanguage")
-    public ResponseEntity<List<ProgrammingLanguageResponse>> programmingLanguageList() {
+    public ResponseEntity<List<ProgrammingLanguageResponse>> programmingLanguageList(
+            @SessionAttribute(name="loginUser", required = false) User user
+    ) {
         List<ProgrammingLanguageResponse> programmingLanguageResponses = gameService.findProgrammingLanguage();
         List<ProgrammingLanguageResponse> result = programmingLanguageResponses.stream()
                 .map(programmingLanguageResponse ->
                         programmingLanguageResponse.toBuilder()
-                                .progress(progressService.findProgressByLanguage(1L, programmingLanguageResponse.getId()))
+                                .progress(progressService.findProgressByLanguage(user.getId(), programmingLanguageResponse.getId()))
                                 .build()
                 ).toList();
 
@@ -36,16 +40,18 @@ public class GameController {
     @GetMapping("/programmingLanguage/{programmingLanguageId}/categories/{categoryId}")
     public ResponseEntity<CategoryResponse> categoryDetails(
             @PathVariable Long programmingLanguageId,
-            @PathVariable Long categoryId) {
+            @PathVariable Long categoryId
+    ) {
         return ResponseEntity.ok(gameService.findCategory(categoryId));
     }
 
     @GetMapping("/programmingLanguage/{programmingLanguageId}/categories")
     public ResponseEntity<List<CategoryResponse>> categoryList(
-            @PathVariable Long programmingLanguageId
+            @PathVariable Long programmingLanguageId,
+            @SessionAttribute(name="loginUser", required = false) User user
     ) {
         List<CategoryResponse> categoryResponses = gameService.findCategory();
-        List<Double> progresses = progressService.findProgressByCategory(1L, programmingLanguageId);
+        List<Double> progresses = progressService.findProgressByCategory(user.getId(), programmingLanguageId);
 
         List<CategoryResponse> result = IntStream.range(0, categoryResponses.size())
                 .mapToObj(index -> categoryResponses.get(index).toBuilder()

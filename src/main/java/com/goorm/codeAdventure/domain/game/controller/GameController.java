ff50f1692953bcc80ee4bebe46f44832849c3ea7
jae woo Jung <awfjol2008@gmail.com>
2024-06-18 17:14:00 +0900

    feat: 기존 프로그래밍 언어, 카테고리 조회 로직에 진행도 필드 추가 작업 진행

--- src/main/java/com/goorm/codeAdventure/domain/game/controller/GameController.java
+++ src/main/java/com/goorm/codeAdventure/domain/game/controller/GameController.java
@@ -4,6 +4,7 @@ import com.goorm.codeAdventure.domain.game.dto.response.CategoryResponse;
 import com.goorm.codeAdventure.domain.game.dto.response.ProgrammingLanguageResponse;
 import com.goorm.codeAdventure.domain.game.dto.response.StageResponse;
 import com.goorm.codeAdventure.domain.game.service.GameService;
+import com.goorm.codeAdventure.domain.game.service.ProgressService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,15 +12,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Controller
 @RequiredArgsConstructor
 public class GameController {
     private final GameService gameService;
+    private final ProgressService progressService;
 
     @GetMapping("/programmingLanguage")
     public ResponseEntity<List<ProgrammingLanguageResponse>> programmingLanguageList() {
-        return ResponseEntity.ok(gameService.findProgrammingLanguage());
+        List<ProgrammingLanguageResponse> programmingLanguageResponses = gameService.findProgrammingLanguage();
+        List<ProgrammingLanguageResponse> result = programmingLanguageResponses.stream()
+                .map(programmingLanguageResponse ->
+                        programmingLanguageResponse.toBuilder()
+                                .progress(progressService.findProgressByLanguage(1L, programmingLanguageResponse.getId()))
+                                .build()
+                ).toList();
+
+        return ResponseEntity.ok(result);
     }
 
     @GetMapping("/programmingLanguage/{programmingLanguageId}/categories/{categoryId}")
@@ -33,9 +44,17 @@ public class GameController {
     public ResponseEntity<List<CategoryResponse>> categoryList(
             @PathVariable Long programmingLanguageId
     ) {
-        return ResponseEntity.ok(gameService.findCategory());
-        // 어차피 언어별로도 카테고리 종류는 똑같아서 사실 pathVariable을 막 쓰지는 않음. 밑의 경우에 씀
-        // programmingLanguage가 java인 stages들을 모아서 progress 연산을 통해 해당 카테고리의 총 진척도를 확인할 수 있도록 구현
+        List<CategoryResponse> categoryResponses = gameService.findCategory();
+        List<Double> progresses = progressService.findProgressByCategory(1L, programmingLanguageId);
+
+        List<CategoryResponse> result = IntStream.range(0, categoryResponses.size())
+                .mapToObj(index -> categoryResponses.get(index).toBuilder()
+                        .progress(progresses.get(index))
+                        .build()
+                )
+                .toList();
+
+        return ResponseEntity.ok(result);
     }
 
     @GetMapping("/programmingLanguage/{programmingLanguageId}/categories/{categoryId}/stages")

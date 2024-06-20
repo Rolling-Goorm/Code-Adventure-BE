25434f2ce2c59406f455157a8b6550b4c17a1567
jae woo Jung <awfjol2008@gmail.com>
2024-06-20 13:45:27 +0900

    feat: 사용자 해결 문제수 조회 메서드 구현

--- src/main/java/com/goorm/codeAdventure/domain/game/repository/ProgressRepository.java
+++ src/main/java/com/goorm/codeAdventure/domain/game/repository/ProgressRepository.java
@@ -8,13 +8,24 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ProgressRepository extends JpaRepository<Progress, Long> {
-    @Query("select count(p) " +
-            "from Progress p " +
-            "where p.user.id = ?1 and p.stage.programmingLanguage = ?2 and p.stage.category = ?3 and p.attemptResult = ?4")
-    Integer countSuccessfulStagesByCategory(
+    @Query("""
+            select count(p)
+            from Progress p
+            where p.user.id = ?1 and p.stage.programmingLanguage = ?2 and p.stage.category = ?3 and p.attemptResult = ?4""")
+    Integer countStagesByCategory(
             Long userId,
             ProgrammingLanguage programmingLanguage,
             Category category,
             AttemptResult attemptResult
     );
+
+    boolean existsByUser_IdAndStage_Problem_IdAndAttemptResult(Long id, Long id1, AttemptResult attemptResult);
+
+    // 사용자가 특정 문제를 푼 적이 있는지 확인 -> 있으면 코인 안줘버려
+    @Query("""
+            select (count(p) > 0) from Progress p
+            where p.user.id = ?1 and p.stage.problem.id = ?2 and p.attemptResult = ?3""")
+    boolean isProgressExistByProblem(Long userId, Long problemId, AttemptResult attemptResult);
+
+
 }

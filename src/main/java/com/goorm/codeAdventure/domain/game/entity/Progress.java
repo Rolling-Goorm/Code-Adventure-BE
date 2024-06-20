8b1c2be6c5385bdabefe14e2633eb9a5ffeb86e1
jae woo Jung <awfjol2008@gmail.com>
2024-06-20 13:44:45 +0900

    feat: Progress 엔티티에 누락된 Enum 필드 설정 추가

--- src/main/java/com/goorm/codeAdventure/domain/game/entity/Progress.java
+++ src/main/java/com/goorm/codeAdventure/domain/game/entity/Progress.java
@@ -3,6 +3,8 @@ package com.goorm.codeAdventure.domain.game.entity;
 import com.goorm.codeAdventure.domain.problem.entity.AttemptResult;
 import com.goorm.codeAdventure.domain.user.entity.User;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -29,6 +31,7 @@ public class Progress {
     @JoinColumn(name = "stage_id")
     private Stage stage;
 
+    @Enumerated(EnumType.STRING)
     private AttemptResult attemptResult;
 
     public Progress(User user, Stage stage, AttemptResult attemptResult) {

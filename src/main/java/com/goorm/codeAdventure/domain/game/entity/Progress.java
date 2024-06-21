9b00aeea8057021d5f73594ba009956b1c310ff3
jae woo Jung <awfjol2008@gmail.com>
2024-06-21 15:40:11 +0900

    feat: Problem 엔티티 필드에 지연 로딩 옵션 추가

--- src/main/java/com/goorm/codeAdventure/domain/game/entity/Progress.java
+++ src/main/java/com/goorm/codeAdventure/domain/game/entity/Progress.java
@@ -5,6 +5,7 @@ import com.goorm.codeAdventure.domain.user.entity.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -23,11 +24,11 @@ public class Progress {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "stage_id")
     private Stage stage;
 

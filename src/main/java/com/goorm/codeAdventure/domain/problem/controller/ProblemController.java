f85702605c7dc5e3adfaaccc4c26601a31e09fb2
jae woo Jung <awfjol2008@gmail.com>
2024-06-21 16:05:19 +0900

    fix: 기존 세션조회 로직 제거 및 임시 로직 구현

--- src/main/java/com/goorm/codeAdventure/domain/problem/controller/ProblemController.java
+++ src/main/java/com/goorm/codeAdventure/domain/problem/controller/ProblemController.java
@@ -1,30 +1,39 @@
 package com.goorm.codeAdventure.domain.problem.controller;
 
+import com.goorm.codeAdventure.domain.problem.dto.request.SolveRequest;
 import com.goorm.codeAdventure.domain.problem.dto.response.ProblemResponse;
+import com.goorm.codeAdventure.domain.problem.dto.response.SolveResponse;
 import com.goorm.codeAdventure.domain.problem.service.ProblemService;
 import com.goorm.codeAdventure.domain.user.entity.User;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
+import com.goorm.codeAdventure.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequiredArgsConstructor
 public class ProblemController {
     private final ProblemService problemService;
+    private final UserService userService;
 
     @GetMapping("/problems/{problemId}")
-    public ResponseEntity<ProblemResponse> problemDetails(@PathVariable Long problemId) {
-        return ResponseEntity.ok(problemService.findProblem(problemId));
+    public ResponseEntity<ProblemResponse> problemDetails(
+            @PathVariable Long problemId
+    ) {
+        User user = userService.findOne(1L);
+        return ResponseEntity.ok(problemService.findProblem(problemId, user));
     }
 
     @PostMapping("/problems/{problemId}")
-    public ResponseEntity<String> solveProblem(@PathVariable Long problemId, @RequestBody String sourceCode,
-                                               @SessionAttribute(name = "loginUser", required = false) User user) throws Exception {
-        problemService.solveCode(problemId, sourceCode, user);
-
-        return ResponseEntity.ok("");
+    public ResponseEntity<SolveResponse> solveProblem(
+            @PathVariable Long problemId,
+            @RequestBody SolveRequest request
+    ) {
+        User user = userService.findOne(1L);
+        return ResponseEntity.ok(problemService.solveCode(problemId, request.getSourceCode(), user));
     }
 }
